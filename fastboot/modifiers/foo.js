3fa4437cabcb1e5a1fedb498c8c9cb28a8138971
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-01-16 17:26:16 +0100

    Add a possible workaround

--- fastboot/modifiers/foo.js
+++ fastboot/modifiers/foo.js
@@ -1,4 +1,6 @@
 import { modifier } from 'ember-modifier';
+// Uncommenting this fixes the problem
+// import 'fastboot-embroider-repro/modifiers/foo.css';
 
 export default modifier(function foo(/* element, params, hash*/) {
   console.log('fastboot overriden foo');
